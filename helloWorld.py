a99eff9e7ca0c668d59a0f5988c53e08b0162b33
Rogerio Lima <rogerio.lima@BRSP5JJ1GJAMX.local>
2019-10-18 12:15:20 -0300

    Primeiro commit

---
+++ helloWorld.py
@@ -0,0 +1,15 @@
+# This Python file uses the following encoding: utf-8
+import os, sys
+print('')
+print('')
+print('APRENDENDO PYTHON')
+print('')
+
+name = ''
+name = input('Whats your name?')
+print('')
+
+print('Seu nome é: ' + name)
+
+print('')
+print('')

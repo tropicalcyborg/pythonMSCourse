c3d56ff1ed38642ddcd36459fbf375196ed5a210
Rogerio Lima <rogerio.lima@BRSP5JJ1GJAMX.local>
2019-12-09 12:56:02 -0300

    atualizando arquivos

--- transform.py
+++ transform.py
@@ -1,122 +1,65 @@
+
+# import the necessary packages
 import numpy as np
 import cv2
-import argparse
-from colorama import init, Fore
-init()
-import imutils
+ 
+def order_points(pts):
+	# initialzie a list of coordinates that will be ordered
+	# such that the first entry in the list is the top-left,
+	# the second entry is the top-right, the third is the
+	# bottom-right, and the fourth is the bottom-left
+	rect = np.zeros((4, 2), dtype = "float32")
+ 
+	# the top-left point will have the smallest sum, whereas
+	# the bottom-right point will have the largest sum
+	s = pts.sum(axis = 1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
+ 
+	# now, compute the difference between the points, the
+	# top-right point will have the smallest difference,
+	# whereas the bottom-left will have the largest difference
+	diff = np.diff(pts, axis = 1)
+	rect[1] = pts[np.argmin(diff)]
+	rect[3] = pts[np.argmax(diff)]
+ 
+	# return the ordered coordinates
+	return rect
 
-
-ap = argparse.ArgumentParser()
-ap.add_argument("-n", "--name", required=True,
-    help="name of the user")
-
-args = vars(ap.parse_args())
-
-print()
-print(Fore.RED, "Hi, there {}, its nice to meet you!...".format(args["name"]))
-print()
-# print(args)
-# print()
-Fore.WHITE
-print()
-
-image = cv2.imread("TestImages/dog.jpg")
-(h, w, d) = image.shape
-# print("width={}, height={}, depth={}".format(w, h, d))
-
-# (B, G, R) = image[100, 50]
-# print("R={}, G={}, B={}".format(R, G, B))
-
-cv2.imshow("Cachorro", image)
-# cv2.waitKey()
-
-# cao = image[498:758, 97:270]
-# cv2.imshow("cão", cao)
-# cv2.waitKey(0)
-
-# roi = image[0:101, 60:165]
-# cv2.imshow("vassoura", roi)
-# cv2.waitKey(0)
-
-# resize the image to 200x200px, ignoring aspect ratio
-# resized = cv2.resize(image, (200, 200))
-# cv2.imshow("Fixed Resizing", resized)
-# cv2.waitKey(0)
-
-
-# fixed resizing and distort aspect ratio so let's resize the width
-# to be 300px but compute the new height based on the aspect ratio
-# r = 300.0 / w
-# dim = (300, int(h * r))
-# resized = cv2.resize(image, dim)
-# cv2.imshow("Aspect Ratio Resize", resized)
-# cv2.waitKey(0)
-
-# manually computing the aspect ratio can be a pain so let's use the
-# imutils library instead
-# resized = imutils.resize(image, width=200)
-# cv2.imshow("Imutils Resize", resized)
-# cv2.waitKey(0)
-
-
-# let's rotate an image 45 degrees clockwise using OpenCV by first
-# computing the image center, then constructing the rotation matrix,
-# and then finally applying the affine warp
-#center = (w // 2, h // 2)
-center = (0,h)
-M = cv2.getRotationMatrix2D(center, -90, 1.0)
-rotated = cv2.warpAffine(image, M, (h,h+w))
-
-# rotatedCroped = rotated[h:h+w, 0:h]
-# cv2.imshow("OpenCV Rotation", rotatedCroped)
-# cv2.waitKey(0)
-
-
-# rotation can also be easily accomplished via imutils with less code
-# rotated = imutils.rotate(image, -90)
-# cv2.imshow("Imutils Rotation", rotated)
-# cv2.waitKey(0)
-
-
-# OpenCV doesn't "care" if our rotated image is clipped after rotation
-# so we can instead use another imutils convenience function to help
-# us out
-# rotated = imutils.rotate_bound(image, 45)
-# cv2.imshow("Imutils Bound Rotation", rotated)
-# cv2.waitKey(0)
-
-
-# apply a Gaussian blur with a 11x11 kernel to the image to smooth it,
-# useful when reducing high frequency noise
-# blurred = cv2.GaussianBlur(rotated, (11, 11), 0)
-# cv2.imshow("Blurred", blurred)
-# cv2.waitKey(0)
-
-
-# draw a 2px thick red rectangle surrounding the face
-output = image.copy()
-cv2.rectangle(output, (97,498), (270, 758,), (0, 0, 255), 2)
-# cv2.imshow("Rectangle", output)
-# cv2.waitKey(0)
-
-# draw a blue 20px (filled in) circle on the image centered at
-# x=300,y=150
-# output = image.copy()
-# cv2.circle(output, (178,567), 20, (0, 0, 255), -1)
-# cv2.imshow("Circle", output)
-# cv2.waitKey(0)
-
-
-# # draw a 5px thick red line from x=60,y=20 to x=400,y=200
-# output = image.copy()
-# cv2.line(output, (60, 20), (400, 200), (0, 0, 255), 5)
-# cv2.imshow("Line", output)
-# cv2.waitKey(0)
-
-
-# draw green text on the image
-# output = image.copy()
-cv2.putText(output, "Encontrado", (104, 525), 
-cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
-cv2.imshow("Text", output)
-cv2.waitKey(0)
+def four_point_transform(image, pts):
+	# obtain a consistent order of the points and unpack them
+	# individually
+	rect = order_points(pts)
+	(tl, tr, br, bl) = rect
+ 
+	# compute the width of the new image, which will be the
+	# maximum distance between bottom-right and bottom-left
+	# x-coordiates or the top-right and top-left x-coordinates
+	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+	maxWidth = max(int(widthA), int(widthB))
+ 
+	# compute the height of the new image, which will be the
+	# maximum distance between the top-right and bottom-right
+	# y-coordinates or the top-left and bottom-left y-coordinates
+	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+	maxHeight = max(int(heightA), int(heightB))
+ 
+	# now that we have the dimensions of the new image, construct
+	# the set of destination points to obtain a "birds eye view",
+	# (i.e. top-down view) of the image, again specifying points
+	# in the top-left, top-right, bottom-right, and bottom-left
+	# order
+	dst = np.array([
+		[0, 0],
+		[maxWidth - 1, 0],
+		[maxWidth - 1, maxHeight - 1],
+		[0, maxHeight - 1]], dtype = "float32")
+ 
+	# compute the perspective transform matrix and then apply it
+	M = cv2.getPerspectiveTransform(rect, dst)
+	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+ 
+	# return the warped image
+	return warped
